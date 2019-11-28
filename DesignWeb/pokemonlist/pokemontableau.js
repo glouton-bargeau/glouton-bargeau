56d38a651d9970880ee9fc2cb81f97eb4c86998e
glouton-bargeau <heon.antoine@carrefour.cegepvicto.ca>
2019-11-28 11:52:41 -0500

    colenter

--- DesignWeb/pokemonlist/pokemontableau.js
+++ DesignWeb/pokemonlist/pokemontableau.js
@@ -1,7 +1,7 @@
 /*********************************************************************/
 var pokemons = ["pokemon bug","Bulbizarre", "Herbizarre","Florizarre","Salameche","Reptincel","Dracaufeu","Carapuce","Carabaffe","Tortank","Chenipan","Chrysacier","Papillusion","Aspicot","Coconfort","Dardargnan","Roucool","Roucoups","Roucarnage","Rattata","Rattatac","Piafabec","Rapasdepic","Abo","Arbok","Pikachu","Raichu","Sabelette","Sablaireau","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Melofee","Melodelfe","Goupix","Feunard","Rondoudou","Grodoudou","Nosferapti","Nosferalto","Mystherbe","Ortide","Rafflesia","Paras","Parasect","Mimitoss","Aeromite","Taupiqueur","Triopikeur","Miaouss","Persian","Psykokwak","Akwakwak","Ferosinge","Colossinge","Caninos","Arcanin","Ptitard","Tetarte","Tartard","Abra","Kadabra","Alakazam","Machoc","Machopeur","Mackogneur","Chetiflor","Boustiflor","Empiflor","Tentacool","Tentacruel","Racaillou","Gravalanch","Grolem","Ponyta","Galopa","Ramoloss","Flagadoss","Magneti","Magneton","Canarticho","Doduo","Dodrio","Otaria","Lamantine","Tadmorv","Grotadmorv","Kokiyas","Crustabri","Fantominus","Spectrum","Ectoplasma","Onix","Soporifik","Hypnomade","Kraby","Krabboss","Voltorbe","Electrode","Noeunoeuf","Noadkoko","Osselait","Ossatueur","Kicklee","Tygnon","Excelangue","Smogo","Smogogo","Rhinocorne","Rhinoferos","Leveinard","Saquedeneu","Kangourex","Hypotrempe","Hypocean","Poissirene","Poissoroy","Stari","Staross","M. Mime","Insecateur","Lippoutou","Elektek","Magmar","Scarabrute","Tauros","Magicarpe","Leviator","Lokhlass","Metamorph","Evoli","Aquali","Voltali","Pyroli","Porygon","Amonita","Amonistar","Kabuto","Kabutops","Ptera","Ronflex","Artikodin","Electhor","Sulfura","Minidraco","Draco","Dracolosse","Mewtwo","Mew"];
-/*********************************************************************/
-$('#go').click(function (dopokemon) {
+/***********************function pokemon******************************/
+function pokemon() {
     /************var*et*random*********/
     var nombre =  Number($('#pkenumber').val().toUpperCase());
     var shiny = Math.round(Math.random()*5);
@@ -43,4 +43,14 @@ $('#go').click(function (dopokemon) {
             $('#pokemonimg').attr("src", "pokemon/" + nombre + ".png");
         }
     }
+}
+/****************************keyup************************************/
+$('#pkenumber').keyup(function (e) {
+    if (e.which === 13) {
+        pokemon();
+    }
+});
+/****************************click************************************/
+$('#go').click(function (e) {
+    pokemon();
 });
