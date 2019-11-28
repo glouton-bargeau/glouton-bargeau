90d19f30881252094e40ded184d917ec8e466ec3
glouton-bargeau <heon.antoine@carrefour.cegepvicto.ca>
2019-11-27 19:24:17 -0500

    alola

--- DesignWeb/pokemonlist/pokemontableau.js
+++ DesignWeb/pokemonlist/pokemontableau.js
@@ -1,7 +1,7 @@
 /*********************************************************************/
 var pokemons = ["pokemon bug","Bulbizarre", "Herbizarre","Florizarre","Salameche","Reptincel","Dracaufeu","Carapuce","Carabaffe","Tortank","Chenipan","Chrysacier","Papillusion","Aspicot","Coconfort","Dardargnan","Roucool","Roucoups","Roucarnage","Rattata","Rattatac","Piafabec","Rapasdepic","Abo","Arbok","Pikachu","Raichu","Sabelette","Sablaireau","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Melofee","Melodelfe","Goupix","Feunard","Rondoudou","Grodoudou","Nosferapti","Nosferalto","Mystherbe","Ortide","Rafflesia","Paras","Parasect","Mimitoss","Aeromite","Taupiqueur","Triopikeur","Miaouss","Persian","Psykokwak","Akwakwak","Ferosinge","Colossinge","Caninos","Arcanin","Ptitard","Tetarte","Tartard","Abra","Kadabra","Alakazam","Machoc","Machopeur","Mackogneur","Chetiflor","Boustiflor","Empiflor","Tentacool","Tentacruel","Racaillou","Gravalanch","Grolem","Ponyta","Galopa","Ramoloss","Flagadoss","Magneti","Magneton","Canarticho","Doduo","Dodrio","Otaria","Lamantine","Tadmorv","Grotadmorv","Kokiyas","Crustabri","Fantominus","Spectrum","Ectoplasma","Onix","Soporifik","Hypnomade","Kraby","Krabboss","Voltorbe","Electrode","Noeunoeuf","Noadkoko","Osselait","Ossatueur","Kicklee","Tygnon","Excelangue","Smogo","Smogogo","Rhinocorne","Rhinoferos","Leveinard","Saquedeneu","Kangourex","Hypotrempe","Hypocean","Poissirene","Poissoroy","Stari","Staross","M. Mime","Insecateur","Lippoutou","Elektek","Magmar","Scarabrute","Tauros","Magicarpe","Leviator","Lokhlass","Metamorph","Evoli","Aquali","Voltali","Pyroli","Porygon","Amonita","Amonistar","Kabuto","Kabutops","Ptera","Ronflex","Artikodin","Electhor","Sulfura","Minidraco","Draco","Dracolosse","Mewtwo","Mew"];
 /*********************************************************************/
-$('#go').click(function () {
+$('#go').click(function (dopokemon) {
     /************var*et*random*********/
     var nombre =  Number($('#pkenumber').val().toUpperCase());
     var shiny = Math.round(Math.random()*5);
@@ -10,7 +10,8 @@ $('#go').click(function () {
     $('#bgpokedex').css("background-image","url(\"pokemon/pokedex ouvert.png\")")
         .css("height","900px");
     $('#margineset').css("margin-top","350px");
-    if (nombre===19) {
+    if (nombre===19||nombre===20||nombre===26||nombre===27){
+
         if (alola === 1) {
             if (shiny === 1) {
                 $('#nom').html("#"+ nombre + "   " + pokemons[nombre]);
@@ -19,14 +20,17 @@ $('#go').click(function () {
                 $('#nom').html("#"+ nombre + "   " + pokemons[nombre]);
                 $('#pokemonimg').attr("src", "pokemon/" + nombre + "a.gif");
             }
-        } else {
+
+        }
+        else {
             if (shiny === 1) {
                 $('#nom').html("#"+ nombre + "   " + pokemons[nombre]);
-                $('#pokemonimg').attr("src", "pokemon/" + nombre + "s.png");
+                $('#pokemonimg').attr("src", "pokemon/" + nombre + "s.gif");
             } else {
                 $('#nom').html("#"+ nombre + "   " + pokemons[nombre]);
                 $('#pokemonimg').attr("src", "pokemon/" + nombre + ".png");
             }
+
         }
     }
     else {
@@ -39,5 +43,4 @@ $('#go').click(function () {
             $('#pokemonimg').attr("src", "pokemon/" + nombre + ".png");
         }
     }
-
 });
